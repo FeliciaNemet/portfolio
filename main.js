a0e4137be0090994981b05a59afbd2aa485ed309
Felicia <nemet.felicia@gmail.com>
2023-02-21 12:18:16 -0500

    added new SSL

--- main.js
+++ main.js
@@ -1,18 +1,18 @@
-const panels = document.querySelectorAll('.panel');
+// const panels = document.querySelectorAll('.panel');
 
-          function toggleOpen() {
-              this.classList.toggle('open');
-          };
+//           function toggleOpen() {
+//               this.classList.toggle('open');
+//           };
 
-          function toggleActive(e) {
-              console.log(e.propertyName);
-              if(e.propertyName.includes('flex')) {
-                    this.classList.toggle('open-active');
-              }
-          }
+//           function toggleActive(e) {
+//               console.log(e.propertyName);
+//               if(e.propertyName.includes('flex')) {
+//                     this.classList.toggle('open-active');
+//               }
+//           }
 
-          panels.forEach(panel => panel.addEventListener('click', toggleOpen));
-          panels.forEach(panel => panel.addEventListener('transitionend', toggleActive));
+//           panels.forEach(panel => panel.addEventListener('click', toggleOpen));
+//           panels.forEach(panel => panel.addEventListener('transitionend', toggleActive));
 
 
 
