17c8c4dbc7cac55c80d85e46b9a663c5f5028f0c
Felicia <nemet.felicia@gmail.com>
2023-02-21 01:40:17 -0500

    many changes made

--- main.js
+++ main.js
@@ -1,3 +1,21 @@
+const panels = document.querySelectorAll('.panel');
+
+          function toggleOpen() {
+              this.classList.toggle('open');
+          };
+
+          function toggleActive(e) {
+              console.log(e.propertyName);
+              if(e.propertyName.includes('flex')) {
+                    this.classList.toggle('open-active');
+              }
+          }
+
+          panels.forEach(panel => panel.addEventListener('click', toggleOpen));
+          panels.forEach(panel => panel.addEventListener('transitionend', toggleActive));
+
+
+
 window.formbutton=window.formbutton||function(){(formbutton.q=formbutton.q||[]).push(arguments)};
   /* customize formbutton below*/     
   formbutton("create", {
